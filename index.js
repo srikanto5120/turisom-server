be0b935fa4346cc017a221f1d72e5414baeb2e93
srikanto5120 <shreekantaray@gmail.com>
2021-10-31 11:43:29 +0600

    commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -22,12 +23,45 @@ async function run() {
     const database = client.db("tourism");
     const tourCollection = database.collection("places");
     const hotelCollection = database.collection("hotels");
+    const roomCollection = database.collection("room");
+    const bookingCollection = database.collection("booking-information");
     // tour place get api
     console.log("connected");
     app.get("/places", async (req, res) => {
       const result = await tourCollection.find({}).toArray();
       console.log("connected");
 
+      res.send(result);
+    });
+
+    // post booking information
+    app.post("/booking-information", async (req, res) => {
+      bookingCollection.insertOne(req.body).then((result) => {
+        res.send(result.insertedId);
+      });
+    });
+
+    /// get booking information
+    app.get("/booking-information", async (req, res) => {
+      const result = await bookingCollection.find({}).toArray();
+      console.log("connected");
+
+      res.send(result);
+    });
+
+    // get room api
+    app.get("/room", async (req, res) => {
+      const result = await roomCollection.find({}).toArray();
+      console.log("connected");
+
+      res.send(result);
+    });
+
+    //get    single api
+    app.use("/place/singlePlace/:id", async (req, res) => {
+      const id = req.params.id;
+      const cursor = { _id: ObjectId(id) };
+      const result = await tourCollection.findOne(cursor);
       res.send(result);
     });
 
